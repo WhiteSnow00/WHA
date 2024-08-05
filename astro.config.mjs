7b5b0f0e0e8e73217bce46d72b727dac28dba004
WhiteSnow00 <kazenosakura100@gmail.com>
2024-08-05 09:23:36 +0700

    Trying to fix deploy server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/serverless';
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
@@ -8,12 +7,8 @@ import pagefind from "astro-pagefind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    maxDuration: 8,
-  }),
+  site: "https://facebook.com/F.Ena2001",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
+  markdown: {
+  },
 });
