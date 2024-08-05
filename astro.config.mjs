cfd0e9c38b3fbdd807abdd270c4bed196915c183
WhiteSnow00 <kazenosakura100@gmail.com>
2024-08-05 09:28:07 +0700

    Static?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/static';
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://facebook.com/F.Ena2001",
+  site: 'http://localhost:3000',
+  adapter: vercel(),
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
   },
