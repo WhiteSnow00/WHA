e2ac946a8ebfe420a540af25f2a378c382465348
WhiteSnow00 <kazenosakura100@gmail.com>
2026-01-12 21:57:07 +0700

    Hide about

--- src/components/fab.tsx
+++ src/components/fab.tsx
@@ -155,16 +155,6 @@ export function FAB() {
               <ArrowRight className="size-4" />
             </div>
           </Link>
-          <Link
-            className="group relative border-b border-border text-sm cursor-pointer flex items-center gap-3 px-4 py-2 hover:bg-secondary hover:text-secondary-foreground transition-colors"
-            href="/about"
-          >
-            <Info className="size-4" />
-            <span>About</span>
-            <div className="absolute opacity-0 translate-x-1/2 right-4 top-1/2 -translate-y-1/2 group-hover:opacity-100 group-hover:translate-x-0 transition-all">
-              <ArrowRight className="size-4" />
-            </div>
-          </Link>
         </PopoverContent>
       </Popover>
     </>
